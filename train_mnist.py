0a8341548bac9aa1f1f523645e40092247484575
Hussen Abu Hamad <hussen.abu.hamad@gmail.com>
2024-01-27 12:18:43 +0200

    import data sampler from BNP and fic AttrDict (broken since py 3.10)

--- train_mnist.py
+++ train_mnist.py
@@ -30,21 +30,7 @@ import os
 from models import DiT_models
 from diffusion import create_diffusion
 from diffusers.models import AutoencoderKL
-
-###############
-# TEMP replace AttrDict that was broken since 3.10
-###############
-
-from collections import UserDict
-# from attrdict import AttrDict
-
-class AttrDict(UserDict):
-    def __getattr__(self, key):
-        return self.__getitem__(key)
-    def __setattr__(self, key, value):
-        if key == "data":
-            return super().__setattr__(key, value)
-        return self.__setitem__(key, value)
+from data._attrdict import AttrDict
 
 #################################################################################
 #                             Training Helper Functions                         #
